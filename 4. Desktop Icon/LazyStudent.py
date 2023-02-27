33b36b35c04f77f46b670d0ca81c53db90d04cb8
Joel <103588217+joel-quek@users.noreply.github.com>
2023-02-27 16:55:07 +0800

    czcvz

--- 4. Desktop Icon/LazyStudent.py
+++ 4. Desktop Icon/LazyStudent.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 from moviepy import *
 #from moviepy.editor import VideoFileClip
@@ -11,6 +12,7 @@ import numpy as np
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 # long test link https://www.youtube.com/watch?v=TsfLm5iiYb4
 # short test link https://www.youtube.com/watch?v=yCCrS_jynbg 
+# ted talk test link https://www.youtube.com/watch?v=NiKtZgImdlY
 list = []
 
 #Functions
@@ -35,7 +37,16 @@ def download__transcribe_summarise_file():
     # Transcriber
     try:
         screen.title('Download Complete! Transcribing...')
-        model = whisper.load_model('base')
+        selection = combo.get()
+        model = whisper.load_model(selection)
+        '''
+        if model_field.cget("text") == "tiny":
+            model = whisper.load_model('tiny')
+        elif model_field.cget("text") == "base":
+            model = whisper.load_model('base')
+        else:
+            model = whisper.load_model('small')
+        '''
         model_text = model.transcribe(user_path + r'\downloaded_video', fp16=False)['text'] # works till this line. Gives transcription output
         text_file = open(user_path + r'\transcribed_text.txt', "w")
         text_file.write(model_text)
@@ -57,7 +68,9 @@ def download__transcribe_summarise_file():
         text_file2 = open(user_path + r"\summary.txt", "wt")
         #text_file2.write(summary_text[0]['summary_text'])
         for i in range(len(list)):
-            text_file2.write(list[i])
+            text_file2.write("-"+list[i])
+            text_file2.write("\n")
+            text_file2.write("\n")
         text_file2.close()
         shutil.move(text_file2, user_path)
     except:
@@ -67,7 +80,7 @@ def download__transcribe_summarise_file():
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 screen = Tk()
 title = screen.title('Lazy Student')
-canvas = Canvas(screen, width=500, height=500)
+canvas = Canvas(screen, width=500, height=700)
 canvas.pack()
 
 #image logo
@@ -76,29 +89,44 @@ logo_img = PhotoImage(file='3. Lazy Student GUI\yt3.png')
 logo_img = logo_img.subsample(2, 2)
 canvas.create_image(250, 100, image=logo_img)
 
-#link field
+#link field Model Type
+model_field = Entry(screen, width=40, font=('Arial', 15) )
+model_label = Label(screen, text="Enter Model Type [tiny, base or small]: ", font=('Arial', 15))
+
+#link field youtube URL
 link_field = Entry(screen, width=40, font=('Arial', 15) )
 link_label = Label(screen, text="Enter Download Link: ", font=('Arial', 15))
 
 #Select Path for saving the file
 path_label = Label(screen, text="Select Path For Download", font=('Arial', 15))
 select_btn =  Button(screen, text="Select Path", bg='blue', padx='22', pady='5',font=('Arial', 15), fg='#fff', command=select_path)
-#Add to window
-canvas.create_window(250, 280, window=path_label)
-canvas.create_window(250, 330, window=select_btn)
 
 #Add widgets to window 
-canvas.create_window(250, 170, window=link_label)
-canvas.create_window(250, 220, window=link_field)
+canvas.create_window(250, 170, window=link_label) # "enter download link"
+canvas.create_window(250, 220, window=link_field) # box for download link
+
+#Add to window
+canvas.create_window(250, 280, window=path_label) #"select path for download"
+canvas.create_window(250, 330, window=select_btn) # the button for path selection
+
+# add model selection to window
+canvas.create_window(250, 400, window=model_label) #"Enter Model Type"
+combo = ttk.Combobox(
+    state="readonly", # not allowing the user to enter their own values
+    values=["tiny", "base", "small"]
+)
+canvas.create_window(250, 450, window=combo)
+# canvas.create_window(250, 450, window=model_field)
 
 #Download btns
 download_btn = Button(screen, text="Download File",bg='blue', padx='22', pady='5',font=('Arial', 15), fg='#fff', command=download__transcribe_summarise_file)
 #add to canvas
-canvas.create_window(250, 390, window=download_btn)
+canvas.create_window(250, 520, window=download_btn)
 
 screen.mainloop()
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Source: https://github.com/Maclinz/yt_downloader/blob/master/download.py
 # source: https://www.youtube.com/watch?v=TEATfq6hPIg
 # test link https://www.youtube.com/watch?v=yCCrS_jynbg 
+# Source: https://pythonassets.com/posts/drop-down-list-combobox-in-tk-tkinter/#:~:text=The%20drop%2Ddown%20list%20is,is%20not%20in%20the%20list.
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
